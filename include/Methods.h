6aee8f24c9390abba38562e439f394f14b16eac8
Vitor Greati <greati@ufrn.edu.br>
2017-03-07 21:33:39 -0300

    Include first version of header file for the class of linear system methods

---
+++ include/Methods.h
@@ -0,0 +1,69 @@
+#ifndef _METHODS_
+#define _METHODS_
+
+/**
+ * Contains various numerical methods for
+ * linear systems.
+ *
+ * @author      Vitor Greati, Vinicius Campos, Raquel Oliveira
+ * @date        2017-03-07
+ * @version     1.0
+ * */
+namespace numerical_analysis {
+
+template<typename TField>
+class LinearSystemsMethods {
+
+    public:
+
+        /**
+         * Performs a LU decomposition with partial
+         * pivoting.
+         *
+         * @param source        Matrix to be decomposed.
+         * @param L             Lower triangular matrix after decomposition.
+         * @param U             Upper triangular matrix after decomposition.
+         * */
+        void getLU(const Matrix<TField> & source,
+                   Matrix<TField> & L,
+                   Matrix<TField> & U);
+
+        /**
+         * Performs backward substitution when receives
+         * an upper triangular matrix. Solves the system
+         * Ax = b.
+         *
+         * @param A             Matrix of coefficients.
+         * @param b             Vector.
+         * @param x             Solution of the linear system.
+         * */
+        void backSubstitution(const Matrix<TField> A,
+                              const Matrix<TField> b,
+                              Matrix<TField> x);
+
+        /**
+         * Solve a linear system by LU decomposition only.
+         *
+         * @param A             Matrix of coefficients.
+         * @param b             Vector.
+         * @param x             Solution of the linear system.
+         * */
+        void solveByLU(const Matrix<TField> A,
+                              const Matrix<TField> b,
+                              Matrix<TField> x);
+
+        /**
+         * Solve a linear system by Cholesky decomposition.
+         *
+         * @param A             Matrix of coefficients.
+         * @param b             Vector.
+         * @param x             Solution of the linear system.
+         * */
+        void solveByCholesky(const Matrix<TField> A,
+                              const Matrix<TField> b,
+                              Matrix<TField> x);
+
+};
+
+}
+#endif
