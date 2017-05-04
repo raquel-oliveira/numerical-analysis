e459f78eeb26dc49e88f9c51b716e6e202fd67bb
Vitor Greati <greati@ufrn.edu.br>
2017-05-04 00:30:52 -0300

    Add header for class FunctionZeroesFinder #33

---
+++ include/FunctionZeroesFinder.h
@@ -0,0 +1,116 @@
+#ifndef _ZEROES_FINDER_
+#define _ZEROES_FINDER_
+
+namespace numerical_analysis {
+
+	/*!
+	 * Contains methods for finding zeroes of functions.
+	 * 
+	 * @author Vitor Greati, Vinicius Campos, Raquel Oliveira
+	 * @date 2017-03-23
+	 * @version 1.0
+	 * */
+	template<typename TField>
+	class FunctionZeroesFinder {
+
+		public:
+
+			/*!
+			 * Enumeration that represent possible
+			 * stop criteria for the methods.
+			 * */
+			enum StopCriteria {
+				IMAGE = 1,				/*!< |f(a)| < err  */
+				DELTA_IMAGE = 2,		/*!< |f(b) - f(a)| < err */
+				DELTA_DOMAIN = 4		/*!< |b - a| < err */	
+			};
+
+			/*!
+			 * Gives Lagrange bounds for a polynomial function.
+			 * 
+			 * @param coeff			List of coefficients a0,...,an
+			 * @param bounds		Tuple of bounds, in the form
+			 *						[lowerneg upperneg lowerpos upper pos].
+			 * */
+			static void lagrange_restriction(std::vector<TField> coeff, 
+					std::tuple<TField, TField, TField, TField> & bounds);
+
+			/*!
+			 * Gives a restriction of the given interval based on the function's
+			 * signal change.
+			 * 
+			 * @param f				The function.
+			 * @param interval		Interval of the domain to be restricted.
+			 * */
+			static void signal_change_restriction(std::function<TField (const & TField)> f,
+					std::pair<TField, TField> & interval);
+			
+			/*!
+			 * Computes a root (zero) of the function given some domain
+			 * interval, using the Bisection Method.
+			 * 
+			 * @param f				The function.
+			 * @param interval		Domain interval.
+			 * @param criteria		Stop criteria. Can be inserted as disjunction.
+			 * @param error			Acceptable error (epsilon).
+			 * @param root			The root found.
+			 * */
+			static void bisection(std::function<TField (const & TField)> f,
+					const std::pair<TField, TField> & interval, 
+					int criteria, const double & error,
+					TField & root); 
+		
+			/*!
+			 * Computes a root (zero) of the function given some domain
+			 * interval, using the Secant Method.
+			 * 
+			 * @param f				The function.
+			 * @param interval		Domain interval.
+			 * @param criteria		Stop criteria. Can be inserted as disjunction.
+			 * @param error			Acceptable error (epsilon).
+			 * @param root			The root found.
+			 * */
+			static void secant(std::function<TField (const & TField)> f,
+					const std::pair<TField, TField> & interval, 
+					int criteria, const double & error,
+					TField & root); 
+	
+			/*!
+			 * Computes a root (zero) of the function given some domain
+			 * interval, using the Newton Method.
+			 * 
+			 * @param f				The function.
+			 * @param df			Function's first derivative.
+			 * @param interval		Domain interval.
+			 * @param criteria		Stop criteria. Can be inserted as disjunction.
+			 * @param error			Acceptable error (epsilon).
+			 * @param root			The root found.
+			 * */
+			static void newton(std::function<TField (const & TField)> f,
+					std::function<TField (const & TField)> df,
+					const std::pair<TField, TField> & interval, 
+					int criteria, const double & error,
+					TField & root); 
+
+			/*!
+			 * Computes a root (zero) of the function given some domain
+			 * interval, using the Fixed Point Method.
+			 * 
+			 * @param g				The function.
+			 * @param dg			Function's first derivative.
+			 * @param interval		Domain interval.
+			 * @param criteria		Stop criteria. Can be inserted as disjunction.
+			 * @param error			Acceptable error (epsilon).
+			 * @param root			The root found.
+			 * */
+			static void fixed_point(std::function<TField (const & TField)> g,
+					std::function<TField (const & TField)> dg,
+					const std::pair<TField, TField> & interval, 
+					int criteria, const double & error,
+					TField & root); 
+	
+	};
+
+};
+
+#endif
