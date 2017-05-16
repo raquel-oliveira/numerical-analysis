df8285aa62bc63cbfa151b7f79780a1b71c5d8cd
Vinícius Campos <viniciuscampos120@gmail.com>
2017-05-15 22:57:35 -0300

    Implement Newton's method in FunctionZeroesFinder. #28

--- src/FunctionZeroesFinder.inl
+++ src/FunctionZeroesFinder.inl
@@ -1,4 +1,6 @@
 #include "FunctionZeroesFinder.h"
+#include <bitset>
+#include <climits>
 
 template<typename TField>
 void numerical_analysis::FunctionZeroesFinder<TField>::lagrange_restriction(std::vector<TField> coeff, 
@@ -38,6 +40,35 @@ void numerical_analysis::FunctionZeroesFinder<TField>::newton(std::function<TFie
 		TField & root,
 		const int iterations) {
 
+	std::bitset<4> bits(criteria); 
+
+	TField p, p0;
+	p0 = aproximation;
+
+	for(auto i = 0; i < iterations; ++i){
+		p = p0 - f(p0) / df(p0);
+		
+		if(bits[0]){
+			if(f(p) < error){
+				root = p;
+				return;
+			}
+		}else if(bits[1]){
+			if(abs(f(p)-f(p0)) < error){
+				root = p;
+				return;
+			}
+		}else if(bits[2]){
+			if(abs(p-p0) < error){
+				root = p;
+				return;
+			}
+		}
+
+		p0 = p;
+	}
+
+	root = INT_MAX;
 }
 
 template<typename TField>
--- test/function_zeroes/find_zeroes.cpp
+++ test/function_zeroes/find_zeroes.cpp
@@ -1,29 +1,39 @@
 #include "FunctionZeroesFinder.h"
 #include <functional>
 #include <iostream>
+#include <cmath>
 
 int main(void) {
 
 	// Your function
-	std::function<double (const double &)> f = [](const double & x){return x*x - 2;};
+	std::function<double (const double &)> f = [](const double & x){return cos(x) - x;};
+	// Your derivative function
+	std::function<double (const double &)> df = [](const double & x){return -sin(x) - 1;};
 
 	// The root we want
 	double root;
 
 	// Domain interval
-	std::pair<double, double> interval;
+	//std::pair<double, double> interval;
 	// TODO Apply methods for interval restriction here
+
+	double aproximation = 3.14159265/4;
 
 	// Max iterations
 	int max_ite = 10;
 
 	// Bisection Method
-	numerical_analysis::FunctionZeroesFinder<double>::bisection(
-		f, 
-		interval, 
+	numerical_analysis::FunctionZeroesFinder<double>::newton(
+		f,
+		df, 
+		aproximation, 
 		numerical_analysis::FunctionZeroesFinder<double>::StopCriteria::IMAGE | numerical_analysis::FunctionZeroesFinder<double>::StopCriteria::DELTA_IMAGE,
-		0.001, root, max_ite
+		0.001, 
+		root, 
+		max_ite
 	);	
+
+	std::cout << "Root: " << root << std::endl;
 
 	return 0;
 }
