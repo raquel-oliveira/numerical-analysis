b8a503fe3e93ae91d0a94886199e2258476a2849
Vitor Greati <greati@ufrn.edu.br>
2017-05-21 14:51:28 -0300

    Implement basin of attraction for n=3

--- test/function_zeroes/basin_attraction.cpp
+++ test/function_zeroes/basin_attraction.cpp
@@ -6,8 +6,8 @@
 #include <cmath>
 #include <exception>
 #include <iomanip>
+#include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-
 
 using namespace numerical_analysis;
 using namespace std::complex_literals;
@@ -26,28 +26,69 @@ int main(void) {
 	std::function<std::complex<double> (const std::complex<double> &)> df = 
 		[](const std::complex<double> & z){return 3. * std::pow(z,2);};
 	
+	std::vector<std::complex<double>> roots = {
+		std::complex<double>(1.0,0.0),
+		std::complex<double>(-1.0/2, std::sqrt(3)/2),
+		std::complex<double>(-1.0/2, -std::sqrt(3)/2)
+	};
+
+	std::vector<cv::Scalar> colors = {
+		{0, 183, 15},
+		{154, 79, 0},
+		{0,0,196}
+	};
+
 	std::complex<double> root;
-	std::complex<double> initial = 1. + 1i;
-	int N0 = 1000;
+	int N0 = 50;
 
-	try{
-		// Newton Method
-		numerical_analysis::FunctionZeroesFinder<std::complex<double>, CLess>::newton(
-			f,
-			df, 
-			initial, 
-			numerical_analysis::FunctionZeroesFinder<std::complex<double>>::StopCriteria::IMAGE | numerical_analysis::FunctionZeroesFinder<std::complex<double>>::StopCriteria::DELTA_IMAGE,
-			0.000001, 
-			root, 
-			N0 
-		);
+	// Image
+	cv::Mat M {1000, 1000, CV_8UC3, cv::Scalar(0,0,0)};
 
-		std::cout << "Newton's root: " << root << std::endl;
+	for (double i = 0; i < 1000; ++i) {
+		for (double j = 0; j < 1000; ++j) {
+			
+			double ii = (2*i)/1000 - 1;
+			double jj = (2*j)/1000 - 1;
 
-		std::cout << "Check: " << f(root) << std::endl;
-	}catch (std::exception& e){
-    	std::cout << e.what();
-    }
+			std::complex<double> initial {ii, jj};
+			
+			try{
+				// Newton Method
+				numerical_analysis::FunctionZeroesFinder<std::complex<double>, CLess>::newton(
+					f,
+					df, 
+					initial, 
+					numerical_analysis::FunctionZeroesFinder<std::complex<double>>::StopCriteria::IMAGE |
+				   	numerical_analysis::FunctionZeroesFinder<std::complex<double>>::StopCriteria::DELTA_IMAGE,
+					0.001, 
+					root, 
+					N0 
+				);
+				
+				// Check what root
+				
+				for (int k = 0; k < roots.size(); ++k) {
+					if (std::abs(std::norm<double>(root - roots[k])) < 0.001) {
+						cv::Vec3b & color = M.at<cv::Vec3b>(i, j);
+						color[0] = colors[k][0];
+						color[1] = colors[k][1];
+						color[2] = colors[k][2];
+						break;
+					}
+				}
+			}catch (std::exception& e){
+				cv::Vec3b & color = M.at<cv::Vec3b>(i, j);
+				color[0] = 255;	
+				color[1] = 255;	
+				color[2] = 255;	
+			}
+		}
+	}
+
+	cv::imshow("test", M);
+	cv::waitKey(0);
+
+	cv::imwrite("~/n3basin.jpg", M);
 
 	return 0;
 }
