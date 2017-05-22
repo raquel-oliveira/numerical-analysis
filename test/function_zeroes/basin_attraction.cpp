2af2b995972717275422c15e9d198b3b5944882c
Vitor Greati <greati@ufrn.edu.br>
2017-05-22 01:01:28 -0300

    Generalize code for any degree

--- test/function_zeroes/basin_attraction.cpp
+++ test/function_zeroes/basin_attraction.cpp
@@ -1,8 +1,12 @@
 #include <complex>
 #include "FunctionZeroesFinder.h"
 #include <functional>
+#include <set>
+#include <unordered_set>
+#include <string>
 #include <iostream>
 #include "Matrix.h"
+#include <cstdlib>
 #include <cmath>
 #include <exception>
 #include <iomanip>
@@ -12,30 +16,44 @@
 using namespace numerical_analysis;
 using namespace std::complex_literals;
 
+
+double precision = 0.001;
+
 struct CLess {
 	bool operator()(std::complex<double> z, double e){
 		return std::norm<double>(z) < e;
 	};
 };
 
-int main(void) {
+struct CLessSet {
+	bool operator()(std::complex<double> z1, std::complex<double> z2){
+		return (std::abs(std::norm<double>(z1- z2)) < precision);
+	};
+};
+
+
+int main(int argn, char * args[]) {
+
+	int n = atoi(args[1]);
+	precision = std::pow(10,-atoi(args[2]));
 
 	std::function<std::complex<double> (const std::complex<double> &)> f = 
-		[](const std::complex<double> & z){return std::pow(z,3) - (1.);};
+		[&](const std::complex<double> & z){return std::pow(z,n) - (1.);};
 
 	std::function<std::complex<double> (const std::complex<double> &)> df = 
-		[](const std::complex<double> & z){return 3. * std::pow(z,2);};
+		[&](const std::complex<double> & z){return std::complex<double>(n + 0.0, 0.0) * std::pow(z,n-1);};
 	
-	std::vector<std::complex<double>> roots = {
-		std::complex<double>(1.0,0.0),
-		std::complex<double>(-1.0/2, std::sqrt(3)/2),
-		std::complex<double>(-1.0/2, -std::sqrt(3)/2)
-	};
+	std::vector<std::pair<std::complex<double>, int>> roots_of_unity;
 
 	std::vector<cv::Scalar> colors = {
 		{0, 183, 15},
 		{154, 79, 0},
-		{0,0,196}
+		{0,0,196},
+		{0,255,20},
+		{130,40,196},
+		{20,80,90},
+		{255,255,0},
+		{0,234, 30}
 	};
 
 	std::complex<double> root;
@@ -60,23 +78,37 @@ int main(void) {
 					initial, 
 					numerical_analysis::FunctionZeroesFinder<std::complex<double>>::StopCriteria::IMAGE |
 				   	numerical_analysis::FunctionZeroesFinder<std::complex<double>>::StopCriteria::DELTA_IMAGE,
-					0.001, 
+					precision, 
 					root, 
 					N0 
 				);
 				
-				// Check what root
-				
-				for (int k = 0; k < roots.size(); ++k) {
-					if (std::abs(std::norm<double>(root - roots[k])) < 0.001) {
+				std::pair<std::set<std::pair<std::complex<double>, int>>::iterator, bool> root_set;
+				// Add to roots
+				CLessSet comp;
+				if (std::norm<double>(f(root)) < precision) {
+					bool found = false;
+					for (auto r : roots_of_unity) {
+						if (comp(r.first, root)) {
+							found = true;
+							cv::Vec3b & color = M.at<cv::Vec3b>(i, j);
+							color[0] = colors[r.second][0];
+							color[1] = colors[r.second][1];
+							color[2] = colors[r.second][2];
+							break;
+						}
+					}
+					if (!found) {
+						roots_of_unity.push_back(std::make_pair(root, roots_of_unity.size()));
 						cv::Vec3b & color = M.at<cv::Vec3b>(i, j);
-						color[0] = colors[k][0];
-						color[1] = colors[k][1];
-						color[2] = colors[k][2];
-						break;
+						color[0] = colors[roots_of_unity.size() - 1][0];
+						color[1] = colors[roots_of_unity.size() - 1][1];
+						color[2] = colors[roots_of_unity.size() - 1][2];
 					}
 				}
-			}catch (std::exception& e){
+
+
+			} catch (std::exception& e){
 				cv::Vec3b & color = M.at<cv::Vec3b>(i, j);
 				color[0] = 255;	
 				color[1] = 255;	
@@ -88,7 +120,7 @@ int main(void) {
 	cv::imshow("test", M);
 	cv::waitKey(0);
 
-	cv::imwrite("~/n3basin.jpg", M);
+	cv::imwrite("/home/vitorgreati/nbasin.jpg", M);
 
 	return 0;
 }
